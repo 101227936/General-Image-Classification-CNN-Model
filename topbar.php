a01874a5e1a3d4a3f09b5cfb4251e643d5c9215b
Harold <55337527+Juicearian@users.noreply.github.com>
2021-04-08 13:20:24 +0800

    Create upload image page layout & seperate the menubar & footer

---
+++ topbar.php
@@ -0,0 +1,24 @@
+  <!-- Topbar Start -->
+  <div class="navbar-custom">
+    <div class="container-fluid">
+        <!-- LOGO -->
+        <div class="logo-box">
+            <a href="home.html" class="logo logo-light text-center">
+                <span class="logo-lg">
+                    <img src="Landing/light-logo.png" alt="" height="40">
+                </span>
+            </a>
+        </div>
+
+        <ul class="list-unstyled topnav-menu topnav-menu-left m-0">  
+            <li class="dropdown d-none d-xl-block">
+                <a class="nav-link dropdown-toggle waves-effect waves-light" href="home.html" role="button">
+                    <i class="mdi mdi-home"></i> 
+                    Home
+                </a>
+            </li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<!-- end Topbar -->
---
+++ upload.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Upload Image - Image Classification System</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="Landing/faviconLogo.ico">
+
+        <!-- Plugins css -->
+        <link href="Admin/dist/assets/libs/dropzone/min/dropzone.min.css" rel="stylesheet" type="text/css" />
+        <link href="Admin/dist/assets/libs/dropify/css/dropify.min.css" rel="stylesheet" type="text/css" />
+
+		<!-- App css -->
+		<link href="Admin/dist/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+		<link href="Admin/dist/assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+
+		<link href="Admin/dist/assets/css/bootstrap-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" disabled />
+		<link href="Admin/dist/assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet"  disabled />
+
+		<!-- icons -->
+		<link href="Admin/dist/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body data-layout-mode=horizontal>
+        <!-- Begin page -->
+        <div id="wrapper">
+            <?php include "topbar.php";?>
+        
+            <!-- ============================================================== -->
+            <!-- Start Page Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page" style="margin:15px; !important">
+                <div class="content">
+                    <!-- Start Content-->
+                    <div class="container-fluid">
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box">
+                                    <h4 class="page-title">Upload your images</h4>
+                                </div>
+                            </div>
+                        </div>     
+                        <!-- end page title --> 
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body" style="padding-bottom:5px;!important">
+										<div class="row">
+											<div class="col-md-6 col-sm-12">
+												<i class="fas fa-question-circle" style="padding-right:5px;margin-bottom:7px;" title="Click 'Class 0' to edit the name of class" data-plugin="tippy" data-tippy-placement="right-start" data-tippy-maxWidth="200px" data-tippy-offset="0, 0"></i>
+												<h4 class="header-title" style="display: inline-block;" id="class_name" contenteditable="true">Class 0</h4>
+                                                <form action="/" method="post" class="dropzone" style="min-height: 0px !important;" id="myAwesomeDropzone" name="myAwesomeDropzone" data-plugin="dropzone" data-previews-container="#file-previews"
+													data-upload-preview-template="#uploadPreviewTemplate">
+													<div class="fallback">
+														<input name="file[]" type="file" multiple />
+													</div>
+													<div class="dz-message needsclick" style="margin:5.5rem;!important">
+														<i class="h1 text-muted dripicons-cloud-upload"></i>
+														<h3>Drop files here or click to upload</h3>
+														<span class="text-muted font-13">(Please upload the images with less than 1MB and maximum of 100 images allowed only)</span>
+													</div>
+												</form>
+											</div>
+											<div class="col-6">
+												<!-- Preview -->
+                                                <i class="fas fa-question-circle" style="padding-right:5px;margin-bottom:7px;" title="Preview your uploaded images" data-plugin="tippy" data-tippy-placement="right-start" data-tippy-maxWidth="200px" data-tippy-offset="0, 0"></i>
+                                                <h4 class="header-title" style="display: inline-block;">Preview Container</h4>
+                                                <div class="dropzone-previews mt-3" id="file-previews" style="margin-top:0.7rem !important; max-height: 328px; overflow-y: auto;"></div>
+											</div>
+										</div>
+                                    </div> <!-- end card-body-->
+									<div class="modal-footer" style="padding-top:0px;!important">
+										<button id='upload' name='upload' class="btn btn-primary waves-effect waves-light float-end">Upload</button>
+									</div>
+                                </div> <!-- end card-->
+                            </div><!-- end col -->
+                        </div>
+                        <!-- end row -->  
+
+                        <!-- file preview template -->
+                        <div class="d-none" id="uploadPreviewTemplate">
+                            <div class="card mt-1 mb-0 shadow-none border">
+                                <div class="p-2">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <img data-dz-thumbnail src="#" class="avatar-sm rounded bg-light" alt="">
+                                        </div>
+                                        <div class="col pl-0">
+                                            <a href="javascript:void(0);" class="text-muted font-weight-bold" data-dz-name></a>
+                                            <p class="mb-0" data-dz-size></p>
+                                        </div>
+                                        <div class="col-auto">
+                                            <!-- Button -->
+                                            <a href="" class="btn btn-link btn-lg text-muted" data-dz-remove>
+                                                <i class="dripicons-cross"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- container -->
+                </div> <!-- content -->
+
+                <!-- Footer Start -->
+                    <?php include "footer.php";?>
+                <!-- end Footer -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Page content -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- END wrapper -->
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- Vendor js -->
+        <script src="Admin/dist/assets/js/vendor.min.js"></script>
+		
+		<!-- Tippy js-->
+        <script src="Admin/dist/assets/libs/tippy.js/tippy.all.min.js"></script>
+
+        <!-- Plugins js -->
+        <script src="Admin/dist/assets/libs/dropzone/min/dropzone.min.js"></script>
+        <script src="Admin/dist/assets/libs/dropify/js/dropify.min.js"></script>
+
+        <!-- App js -->
+        <script src="Admin/dist/assets/js/app.min.js"></script>
+    </body>
+</html>
