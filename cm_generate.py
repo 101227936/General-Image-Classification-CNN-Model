30a7994afc56998c449456aec6813bab1ae18bbb
elaine_0207 <elaine_0207@yahoo.com>
2021-04-25 17:54:58 +0800

    delete python file

--- cm_generate.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\User\AppData\Local\Programs\Python\Python35-32\python.exe
-import sklearn.metrics as metrics
-import itertools
-cm = metrics.confusion_matrix(true_classes, predicted_classes)
-plot_confusion_matrix(cm=cm, classes=class_labels, title='Confusion Matrix', normalize = True)
