9816b47bf9d0d93b73dde0b1846dd5656554b4ba
Harold <55337527+Juicearian@users.noreply.github.com>
2021-04-27 20:41:06 +0800

    Create zip.py

---
+++ zip.py
@@ -0,0 +1,13 @@
+import zipfile
+from os.path import basename
+
+dest_base = './model'
+
+with zipfile.ZipFile(dest_base+'/model.zip','w',compression=zipfile.ZIP_DEFLATED) as zipF:
+    zipF.write(dest_base+'/accuracy.png',basename('/accuracy.png'))
+    zipF.write(dest_base+'/cm.png',basename(dest_base+'/cm.png'))    
+    zipF.write(dest_base+'/label.txt',basename(dest_base+'/label.txt'))
+    zipF.write(dest_base+'/loss.png',basename(dest_base+'/loss.png'))
+    zipF.write(dest_base+'/model.h5',basename(dest_base+'/model.h5'))
+    zipF.write(dest_base+'/report.txt',basename(dest_base+'/report.txt'))
+    zipF.close()
