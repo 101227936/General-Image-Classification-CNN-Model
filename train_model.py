c1821bd59a035726b1c1eb96ab0725b719604d26
101227936 <101227936@students.swinburne.edu.my>
2021-04-29 17:02:50 +0800

    Change indentation of report

--- train_model.py
+++ train_model.py
@@ -312,12 +312,12 @@ score = multiclass_roc_auc_score(validation_generator.classes, predicted_classes
 error_rate = metrics.mean_squared_error(true_classes, predicted_classes)
 
 f = open(dest_base+'/report.txt', "w")
-f.write('Accuracy  : {}\n'.format(np.mean(predicted_classes == true_classes)))
+f.write('Accuracy : {}\n'.format(np.mean(predicted_classes == true_classes)))
 f.write('Precision : {}\n'.format(precision))
-f.write('Recall    : {}\n'.format(recall))
-f.write('F-score   : {}\n'.format(fscore))
-f.write('Score     : {}\n'.format(score))
-f.write('Error Rate: {}\n'.format(error_rate))
+f.write('Recall : {}\n'.format(recall))
+f.write('F-score : {}\n'.format(fscore))
+f.write('Score : {}\n'.format(score))
+f.write('Error Rate : {}\n'.format(error_rate))
 f.close()
 
 shutil.rmtree(TRAIN_DIR)
