187bff1e0aa0e732dca078cd9699843d5a5850ae
Chua Chung Long <101227936@students.swinburne.edu.my>
2021-09-06 12:34:30 +0800

    Configured firebase config and setup firestore rule

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB4gUKsH638lYp0f7Wpv-0LOu5yFJPygOk",
+    authDomain: "testing-4481f.firebaseapp.com",
+    databaseURL: "https://testing-4481f-default-rtdb.firebaseio.com",
+    projectId: "testing-4481f",
+    storageBucket: "testing-4481f.appspot.com",
+    messagingSenderId: "91227991544",
+    appId: "1:91227991544:web:b8ad93751ff8ed95467a0d",
+    measurementId: "G-JSPBSKWV6Z"
+};
+
+firebase.initializeApp(firebaseConfig);
