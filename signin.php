7cc1f330ed110e9fe702f12c37927d2d7ca83def
JasminChu <keechu613@gmail.com>
2021-09-08 09:30:30 +0800

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Sign In - Image Classification System</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="Landing/Logo/faviconLogo.ico">
+
+		<!-- App css -->
+		<link href="template/Template/Admin/dist/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+		<link href="template/Template/Admin/dist/assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+
+		<link href="template/Template/Admin/dist/assets/css/bootstrap-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" disabled />
+		<link href="template/Template/Admin/dist/assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet"  disabled />
+
+        <!-- Sweet Alert-->
+        <link href="template/Template/Admin/dist/assets/libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+
+		<!-- icons -->
+		<link href="template/Template/Admin/dist/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+
+    </head>
+
+    <body class="auth-fluid-pages pb-0">
+
+        <div class="auth-fluid">
+            <!--Auth fluid left content -->
+            <div class="auth-fluid-form-box">
+                <div class="align-items-center d-flex h-100">
+                    <div class="card-body">
+
+                       <!-- Logo -->
+                       <div class="auth-brand text-center text-lg-left">
+                            <div class="auth-logo">
+                                <a href="index.php" class="logo logo-dark text-center">
+                                    <span class="logo-lg">
+                                        <img src="Landing/Logo/dark-logo.png" alt="" height="50">
+                                    </span>
+                                </a>
+            
+                                <a href="index.php" class="logo logo-light text-center">
+                                    <span class="logo-lg">
+                                        <img src="Landing/Logo/light-logo.png" alt="" height="50">
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- title-->
+                        <h4 class="mt-0">Sign In</h4>
+                        <p class="text-muted mb-5">Sign in With Email or Google to access account.</p>
+
+                        <!-- form -->
+                        <form action="#">
+                            <div class="mt-5 form-group text-center">
+                                <button class="btn btn-primary btn-block" type="submit"><i class="mdi mdi-email mr-3"></i>Sign In With Email </button>
+                            </div>
+
+                            <div class="mt-4 form-group text-center">
+                                <button class="btn btn-primary btn-block" type="submit"><i class="mdi mdi-google mr-3"></i>Sign In With Google </button>
+                            </div>
+                        </form>
+                        <!-- end form-->
+
+                        <!-- Footer-->
+                        <footer class="footer footer-alt">
+                            <p class="text-dark">2021 © Image Classification <a href="index.php" class="text-dark ml-1"><b>Return To Landing Page</b></a></p>
+                        </footer>
+
+                    </div> <!-- end .card-body -->
+                </div> <!-- end .align-items-center.d-flex.h-100-->
+            </div>
+            <!-- end auth-fluid-form-box-->
+
+            <!-- Auth fluid right content -->
+            <div class="auth-fluid-right text-center">
+                <div class="auth-user-testimonial">
+                    <h2 class="mb-3 text-white">Image classification</h2>
+                    <p class="lead"><i class="mdi mdi-format-quote-open"></i> the process of categorizing and labeling groups of pixels or vectors within an image based on specific rules. <i class="mdi mdi-format-quote-close"></i>
+                    </p>
+                    <h5 class="text-white">
+                        - The Cognitive Approach in Cloud Computing and Internet of Things Technologies for Surveillance Tracking Systems, 2020
+                    </h5>
+                </div> <!-- end auth-user-testimonial-->
+            </div>
+            <!-- end Auth fluid right content -->
+   
+        </div>
+        <!-- end auth-fluid-->
+
+        <!-- Vendor js -->
+        <script src="template/Template/Admin/dist/assets/js/vendor.min.js"></script>
+
+         <!-- App js -->
+         <script src="template/Template/Admin/dist/assets/js/app.min.js"></script>
+        
+    </body>
+</html>
