cb02db6183d76f311a930104ba695551fe25940d
Harold <55337527+Juicearian@users.noreply.github.com>
2021-09-14 15:52:07 +0800

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -26,7 +26,6 @@
     </head>
 
     <body data-layout-mode="horizontal">
-
         <!-- Begin page -->
         <div id="wrapper">
 
@@ -56,7 +55,7 @@
                         <div class="row">
                             <div class="col-lg-5 col-xl-5">
                                 <div class="card-box text-center">
-                                    <img src="https://www.computerhope.com/jargon/g/guest-user.jpg" id="imageUser" class="rounded-circle avatar-xl img-thumbnail" alt="profile-image">
+                                    <img src="" id="imageUser" class="rounded-circle avatar-xl img-thumbnail" alt="profile-image">
 
                                     <h3 class="mb-0" id="userName">UserName</h4>
                                     <h4 class="mb-0" id="userEmail">useremail@gmail.com</h4>
@@ -85,6 +84,14 @@
                                                             <input type="url" class="form-control" placeholder="Enter your photo url" id="photourl" onkeyup="previewFile(this.value)">
                                                             <div class="input-group-append">
                                                                 <button class="btn btn-dark waves-effect waves-light" type="button" onclick="validatePhotoURL()">Update</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group mb-3">
+                                                        <label>Photo Preview</label>
+                                                        <div class="input-group">
+                                                            <div class="text-center">
+                                                                <img id="image" class="rounded-circle avatar-xl img-thumbnail" src="" alt="Image preview...">
                                                             </div>
                                                         </div>
                                                     </div>
@@ -124,7 +131,7 @@
                                 </div> <!-- end card-box -->
                             </div> <!-- end col-->
 
-                            <div class="col-lg-7 col-xl-7 ">
+                            <div class="col-lg-7 col-xl-7">
                                 <div class="card-box">
                                     <div class="row">
                                         <div class="col-12">
@@ -219,6 +226,308 @@
 
        <!-- App js -->
        <script src="template/Template/Admin/dist/assets/js/app.min.js"></script>
+
+       <script>
+        firebase.auth().onAuthStateChanged(function(user)
+        {
+            document.getElementById('userName').textContent = user.displayName; 
+            document.getElementById('username').value = user.displayName;           //textfield
+
+            document.getElementById('userEmail').textContent = user.email;
+            document.getElementById('email').value = user.email;                    //textfield
+
+            document.getElementById('imageUser').src = user.photoURL;
+            document.getElementById('photourl').value = user.photoURL;              //textfield
+
+            document.getElementById('image').src = user.photoURL;
+
+        });
+       </script>
+
+       <script>
+           function validateUsername()
+           {
+                firebase.auth().onAuthStateChanged(function(user)
+                {
+                    var uname = document.getElementById("username").value;
+                    if (uname != "")
+                    {
+                        const user = firebase.auth().currentUser;
+                        user.updateProfile({
+                            displayName: uname,
+                        }).then(() => {
+                            swal({
+                                title: 'Update Username Success',
+                                type: 'success',
+                                confirmButtonColor: '#6658dd',
+                                backdrop:'#eeeff3',
+                                allowOutsideClick: false,
+                                animation:true,
+                                }).then(function (result) {
+                                    if (result.value) {
+                                        location.reload();
+                                    }
+                                })
+                        }).catch((error) => {
+                            swal({
+                            title: 'Failure',
+                            text: error.message,
+                            type: 'error',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                            }
+                            })
+                        });
+                    }
+                    else
+                    {
+                        swal({
+                            title: 'Update Username Failed',
+                            text:'Username column cannot be empty.',
+                            type: 'error',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                            }
+                        })
+                    }
+                });
+            
+           }
+        
+           var PhotoURLCorrect = true; //bool
+           function previewFile(value)
+           {
+				if(value.length>=0)
+				{
+					var myImageElement = document.getElementById('image');
+					myImageElement.src = 'https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif';
+					var url = value;
+					var img = new Image();
+					img.src = url;
+					img.addEventListener('load', function() {
+                        PhotoURLCorrect = true;
+						var myImageElement = document.getElementById('image');
+						myImageElement.src = url;
+					});
+                    img.addEventListener('error', function() {
+						PhotoURLCorrect = false;
+					});
+				}
+			}
+
+           function validatePhotoURL()
+           {
+                firebase.auth().onAuthStateChanged(function(user)
+                {
+                    var uphotourl = document.getElementById("photourl").value;
+                    if (uphotourl != "")
+                    {
+                        if(PhotoURLCorrect != false)
+                        {
+                            const user = firebase.auth().currentUser;
+                            user.updateProfile({
+                                photoURL: uphotourl,
+                            }).then(() => {
+                                swal({
+                                title: 'Update Photo URL Success',
+                                type: 'success',
+                                confirmButtonColor: '#6658dd',
+                                backdrop:'#eeeff3',
+                                allowOutsideClick: false,
+                                animation:true,
+                                }).then(function (result) {
+                                    if (result.value) {
+                                        location.reload();
+                                    }
+                                })
+                                //
+                            }).catch((error) => {
+                                swal({
+                                title: 'Failure',
+                                text: error.message,
+                                type: 'error',
+                                confirmButtonColor: '#6658dd',
+                                backdrop:'#eeeff3',
+                                allowOutsideClick: false,
+                                animation:true,
+                                }).then(function (result) {
+                                    if (result.value) {
+                                        location.reload();
+                                }
+                                })
+                            });
+                        }
+                        else
+                        {
+                            swal({
+                                title: 'Update Photo URL Failed',
+                                text:'Photo URL incorrect, cannot link to the image.',
+                                type: 'error',
+                                confirmButtonColor: '#6658dd',
+                                backdrop:'#eeeff3',
+                                allowOutsideClick: false,
+                                animation:true,
+                                }).then(function (result) {
+                                    if (result.value) {
+                                        location.reload();
+                                }
+                            })
+                        }
+                    }
+                    else
+                    {
+                        swal({
+                            title: 'Update Photo URL Failed',
+                            text:'Photo URL column cannot be empty.',
+                            type: 'error',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                            }
+                        })
+                    }
+                }); 
+           }
+
+           function validateEmail()
+           {
+                firebase.auth().onAuthStateChanged(function(user)
+                {
+                    var uemail = document.getElementById("email").value;
+                    if (uemail != "")
+                    {
+                        const user = firebase.auth().currentUser;
+
+                        user.updateEmail(uemail).then(() => {
+                            swal({
+                            title: 'Update Email Success',
+                            type: 'success',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            confirmButtonText: 'Yes',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                                }
+                            })
+                        }).catch((error) => {
+                            swal({
+                            title: 'Failure',
+                            text: error.message,
+                            type: 'error',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            confirmButtonText: 'Ok',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                            }
+                            })
+                        });
+                    }
+                    else
+                    {
+                        swal({
+                            title: 'Update Email Failed',
+                            text:'Email column cannot be empty.',
+                            type: 'error',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            confirmButtonText: 'Ok',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                            }
+                        })
+                    }
+                });
+           }
+
+           function validatePassword()
+           {
+                firebase.auth().onAuthStateChanged(function(user)
+                {
+                    var upassword = document.getElementById("password").value;
+                    if (upassword != "")
+                    {
+                        const user = firebase.auth().currentUser;
+
+                        user.updatePassword(upassword).then(() => {
+                            swal({
+                            title: 'Update Password Success',
+                            type: 'success',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            confirmButtonText: 'Yes',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                                }
+                            })
+                        }).catch((error) => {
+                            swal({
+                            title: 'Failure',
+                            text: error.message,
+                            type: 'error',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            confirmButtonText: 'Ok',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                            }
+                            })
+                        });
+                    }
+                    else
+                    {
+                        swal({
+                            title: 'Update Password Failed',
+                            text:'Password column cannot be empty.',
+                            type: 'error',
+                            confirmButtonColor: '#6658dd',
+                            backdrop:'#eeeff3',
+                            confirmButtonText: 'Ok',
+                            allowOutsideClick: false,
+                            animation:true,
+                            }).then(function (result) {
+                                if (result.value) {
+                                    location.reload();
+                            }
+                        })
+                    }
+                });
+           }
+       </script>
+
+        <script>
+			
+		</script>
+
     </body>
 
 </html>
