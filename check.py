fbbbdb43cdfcf3d89b29cdf826ec0e50cf74c14d
JasminChu <keechu613@gmail.com>
2021-09-16 21:32:58 +0800

    Redirect to training page
    Redirect to training page if model training got problem (file/error checking process)

---
+++ check.py
@@ -0,0 +1,28 @@
+import os
+
+def checkFile():
+    count = 0
+    with os.scandir('model/') as entries:
+        for file in entries:
+            print(file.name) #with .name will read 'cm.png', without will read <DirEntry 'loss.png'>
+            if(file.name == 'accuracy.png' or 
+               file.name == 'cm.png' or 
+               file.name == 'label.txt' or
+               file.name == 'loss.png' or
+               file.name == 'model.h5' or
+               file.name == 'report.txt' or
+               file.name == 'roc.png' ):
+                count+=1
+                print('jasmin read file success')
+                continue
+            else:
+                print('jasmin read file fail')
+
+        entries.close()
+
+        if count == 7:
+            print(True)
+        else:
+            print(False)
+            
+checkFile();
--- train_process.php
+++ train_process.php
@@ -8,9 +8,30 @@
 		
 		$result = exec($command, $output, $retval);
 		$result_array = json_decode($result);
-		header('Location: image_predict_train_model.php');
+
+		$command2 = "python check.py";
+		$result2 = exec($command2);
+		if($result2 == 'True')
+		{
+			header('Location: image_predict_train_model.php');
+		}else if ($result2 == 'False')
+		{
+			header('Location: training.php');
+
+		}else
+		{
+			die(header("HTTP/1.0 403 Forbidden"));
+		}
+
+		//header('Location: image_predict_train_model.php');
 	}
-	else die(header("HTTP/1.0 403 Forbidden"));
+
+	else 
+	{
+		header('Location: training.php');
+		//header('Location: image_predict_train_model.php');
+		//die(header("HTTP/1.0 403 Forbidden"));
+	}
 ?>	
 
 
