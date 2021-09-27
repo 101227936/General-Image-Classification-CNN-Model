5c06efbabeed4b92c95e2369fa15ce532fa9f1f0
elaine0207 <elaine_0207@yahoo.com>
2021-09-27 14:12:57 +0800

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -185,52 +185,45 @@
                                                     {?>
                                                     <tbody>
                                                         <tr>
-                                                            <td><a data-toggle="collapse" class="collapsed" href=<?='#collapse'.$i?> role="button" aria-expanded="false" aria-controls="card_accu"><i class="mdi mdi-chevron-down"></i></a></td>
+                                                            <td><a data-toggle="collapse" class="accordion-toggle collapsed" href=<?='#collapse'.$i?> role="button" aria-expanded="false" aria-controls="card_accu"><i class="mdi mdi-chevron-down"></i></a></td>
                                                             <td><?=$his['metadata']?></td>
                                                             <td><?=$his['preTrainedModel']?></td>
                                                             <td><?=$his['epoch']?></td>
                                                             <td><?=$his['batchSize']?></td>
                                                             <td><?=$his['accuracy']?></td>
-                                                    </tr>
+                                                        </tr>
+
+                                                        <tr class="hide-table-padding">
+                                                            <td colspan="3">
+                                                                <div id=<?='collapse'.$i?> class="collapse in p-3">
+                                                                    <div class="row">
+                                                                        <div class="col-4">Precision</div>
+                                                                        <div class="col-3"><?=$his['precision']?></div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">Recall</div>
+                                                                        <div class="col-3"><?=$his['recall']?></div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">F-score</div>
+                                                                        <div class="col-3"><?=$his['fsocre']?></div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">Score</div>
+                                                                        <div class="col-3"><?=$his['score']?></div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">Error Rate</div>
+                                                                        <div class="col-3"><?=$his['errorRate']?></div>
+                                                                    </div>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
                                                     </tbody>
                                                     <?php
                                                     $i++;
                                                     }?>
                                                 </table>
-                                                
-                                                <?php
-                                                $i = 0;
-                                                foreach($history as $his)
-                                                {?>
-                                                <div class="card">
-                                                    <div id=<?='collapse'.$i?> class="collapse bg-light">
-                                                        <div class="card-body text-white">
-                                                        <table id="datatable-buttons2" class="table table-striped dt-responsive nowrap w-100 collapsed">
-                                                            <thead>
-                                                                <tr>
-                                                                    <th>Precision</th>
-                                                                    <th>Recall</th>
-                                                                    <th>F-score</th>
-                                                                    <th>Score</th>
-                                                                    <th>Error Rate</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td><?=$his['precision']?></td>
-                                                                    <td><?=$his['recall']?></td>
-                                                                    <td><?=$his['fscore']?></td>
-                                                                    <td><?=$his['score']?></td>
-                                                                    <td><?=$his['errorRate']?></td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>                                            
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <?php
-                                                $i++;
-                                                }?>
                                             </div>
                                         </div>
                                     </div>
